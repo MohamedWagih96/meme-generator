436a029f962df7c53d8529a39bcba02d14c63de0
MohamedWagih96 <MohamedWagih96@outlook.com>
2020-01-29 02:30:59 +0200

    Updates

---
+++ precache-manifest.dfca86d44d9f96fd70a40f9de54b78fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "873c3213a61dd79b8820dc9a37866f76",
+    "url": "/meme-generator/index.html"
+  },
+  {
+    "revision": "4f2c5c8a4785c82c201d",
+    "url": "/meme-generator/static/css/main.4f6aca8a.chunk.css"
+  },
+  {
+    "revision": "dd5619ab0104875f479f",
+    "url": "/meme-generator/static/js/2.d1c7ed92.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/meme-generator/static/js/2.d1c7ed92.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4f2c5c8a4785c82c201d",
+    "url": "/meme-generator/static/js/main.a197749b.chunk.js"
+  },
+  {
+    "revision": "b8e1865f2d9a05726581",
+    "url": "/meme-generator/static/js/runtime-main.57e0be95.js"
+  }
+]);
