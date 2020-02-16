58f7332d22fd81e99b8a653146402c4ceadac1fa
MohamedWagih96 <MohamedWagih96@outlook.com>
2020-02-16 11:41:49 +0200

    Optimized build code folder

---
+++ build/precache-manifest.c532168b7568641d1131c6633cebf350.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "520b6be8ebb019e622f49c1071c3a509",
+    "url": "/meme-generator/index.html"
+  },
+  {
+    "revision": "89caa54ae9a2e7e19520",
+    "url": "/meme-generator/static/css/main.69f4732e.chunk.css"
+  },
+  {
+    "revision": "dd5619ab0104875f479f",
+    "url": "/meme-generator/static/js/2.d1c7ed92.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/meme-generator/static/js/2.d1c7ed92.chunk.js.LICENSE"
+  },
+  {
+    "revision": "89caa54ae9a2e7e19520",
+    "url": "/meme-generator/static/js/main.668e3722.chunk.js"
+  },
+  {
+    "revision": "b8e1865f2d9a05726581",
+    "url": "/meme-generator/static/js/runtime-main.57e0be95.js"
+  }
+]);
